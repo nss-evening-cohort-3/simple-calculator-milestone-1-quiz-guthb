9787c3536544461af17420846733e8d24efd4bce
Bradley Guthrie <guppy96@gmail.com>
2016-08-15 20:40:31 -0500

    boilerplate

---
+++ SimpleCalculator.Tests/SubtractionTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCalculator.Tests
+{
+    [TestClass]
+    public class SubtractionTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
