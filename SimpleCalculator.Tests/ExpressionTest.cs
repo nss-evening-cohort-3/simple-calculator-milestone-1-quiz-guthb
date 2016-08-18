ef33eff874d9379a57a227a0b1ce79d34048bfc4
Bradley Guthrie <guppy96@gmail.com>
2016-08-18 13:56:10 -0500

    test for patterns pass

--- SimpleCalculator.Tests/ExpressionTest.cs
+++ SimpleCalculator.Tests/ExpressionTest.cs
@@ -7,6 +7,7 @@ namespace SimpleCalculator.Tests
     public class ExpressionTest
     {
         [TestMethod]
+        // can you create a class
         public void ExpresssionICanCreateAnInstance()
         {
             // Arrange
@@ -17,5 +18,48 @@ namespace SimpleCalculator.Tests
             //Assert
             Assert.IsNotNull(my_expression);
         }
+
+        [TestMethod]
+        //can you read a string
+        public void ExpressionIcanReadAString()
+        {
+            Expression my_expression = new Expression();
+
+            Assert.IsNotNull(my_expression.readExpression("test"));
+
+        }
+
+
+        [TestMethod]
+        //can you parse from a string up to the operator char
+        public void ExpressionIcanParseAString()
+        {
+            Expression my_expression = new Expression();
+
+            Assert.IsTrue( "12345" == my_expression.readExpression("12345+67890"));
+
+        }
+
+        [TestMethod]
+        //can you accept a valid string against regex pattern for valid entry
+        public void ExpressionIsAnAcceptedString()
+        {
+            Expression my_expression = new Expression();
+
+            // order of operations + - / % *
+
+            Assert.IsTrue(my_expression.validateEnteredStringCheck("1+2"));
+            Assert.IsTrue(my_expression.validateEnteredStringCheck("1-2"));
+            Assert.IsTrue(my_expression.validateEnteredStringCheck("1/2"));
+            Assert.IsTrue(my_expression.validateEnteredStringCheck("1%2"));
+            Assert.IsTrue(my_expression.validateEnteredStringCheck("1*2"));
+
+            // invalid inputs
+            Assert.IsFalse(my_expression.validateEnteredStringCheck(" 1+2"));
+            Assert.IsFalse(my_expression.validateEnteredStringCheck("1+2 "));
+        }
+        
+
+
     }
 }
--- SimpleCalculator/Expression.cs
+++ SimpleCalculator/Expression.cs
@@ -9,14 +9,79 @@ namespace SimpleCalculator
 {
     public class Expression
     {
-        public Expression()//constructor 
+        public Expression()//constructor best practice
         {
 
         }
 
         public string readExpression(string enteredExpression)
         {
-            return enteredExpression;
+            string[] termsArray;
+            char[] operatorsArray = new char[] { '+', '-', '/', '%', '*' };
+
+            termsArray = enteredExpression.Split(operatorsArray);
+
+            var operatorValues = Regex.Matches(enteredExpression, @"/+|/-|/\|/%|/*");
+           
+            return termsArray[0];
+
+            //got to this point but do not understand how to us IEnumerable<>to capture array of values and operators
         }
+
+        //******trying a different way******//
+        // inclue full pattern to handles digits operators and future constants entered
+        string userInputRegExPattern = @"^(\d*|\w)\s?(\+?\-?\/?\%?\*?)\s?(\d*|\w)$";
+
+        public bool validateEnteredStringCheck(string enteredExpression)
+        {
+            Match match = Regex.Match(enteredExpression, userInputRegExPattern);
+            if (match.Success)
+            {
+                // Handle match here...
+                return true;
+            }
+            else
+            {
+                // Handle no match here...
+                return false;   
+            }    
+            
+        }
+
+
+        //parsing second attempt
+        // method will check to see string is valid
+        // if valid parse the value before the operator , the operator and the last value
+        // if the string is invalid throw execption
+        public void parseStringEntered(string enteredExpression)
+        {
+            if (validateEnteredStringCheck(enteredExpression))
+            {
+                //parsing the first digit
+                userInputBeforeOperator = Convert.ToInt32();
+
+                //parsing the operator
+                userEnteredOperator = Convert.ToChar();
+
+                // parsing the second digit 
+                usertInputAfterOperator = Convert.ToInt32();
+
+            }
+            else
+            {
+                //define a new class to be called for the custom exception
+
+            }
+           
+        
+            
+
+
+
+
+        }
+
+
+        
     }
 }
